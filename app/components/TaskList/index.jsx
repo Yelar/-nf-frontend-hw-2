6773b81eecfd09583d08230eeac8bdfefc896ed9
Yelar <elarysertaj@gmail.com>
2024-06-07 00:37:55 +0500

    first commt

--- app/components/TaskList/index.jsx
+++ app/components/TaskList/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import TaskItem from '../TaskItem';
+import TaskItem from '../TaskItem/index.jsx';
 
-const TaskList = () => {
-  // Render TaskItems using TaskItem component
-  // Filter tasks by status here
+const TaskList = ({ tasks, handleDeleteTask, handleToggleTask, status }) => {
+  const filteredTasks = tasks.filter(task => {
+    if (status === 'all') return true;
+    if (status === 'active') return !task.completed;
+    if (status === 'completed') return task.completed;
+  });
+
   return (
-    <></>
+    <ul>
+      {filteredTasks.map(task => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          handleDeleteTask={handleDeleteTask}
+          handleToggleTask={handleToggleTask}
+        />
+      ))}
+    </ul>
   );
 };
 
